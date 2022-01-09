8e502225492b5c2f15f94bbabaaf779a337bf3c4
Jerzy Sykut <thejurass@gmail.com>
2022-01-09 22:34:35 +0100

    Robot

--- Robot_Pakujący.py
+++ Robot_Pakujący.py
@@ -1,4 +1,4 @@
-liczba_paczek_wyslanych = 1
+liczba_paczek_wyslanych = 0
 suma_kilogrmow_wyslanych = 0
 waga_elementu = 0
 waga_paczki = 0
@@ -9,21 +9,22 @@ numer_paczki = 0
 print("Robot_Pakujacy")
 print("Podaj liczbe elementow do wysylki")
 ilosc_sztuk = int (input())
+liczba_paczek_wyslanych += 1
 while ilosc_sztuk > 0:
     print("Podaj wage elementu")
     waga_elementu = float(input())
     if (waga_elementu == 0 or waga_elementu > 10 or waga_elementu < 1):
         break
     else:
+        suma_kilogrmow_wyslanych += waga_elementu
         if waga_paczki + waga_elementu <= 20:
             #jeszcze upchniemy sie w paczce
             waga_paczki = waga_paczki + waga_elementu
-            suma_kilogrmow_wyslanych += waga_elementu
         else:
-            #sumujemy puste kilogramy z powietrzem ostatniej paczki
+            # sumujemy puste kilogramy z powietrzem ostatniej paczki
             puste_kg_paczki = 20 - waga_paczki
             suma_pustych_kg += puste_kg_paczki
-            #zapamiętujemy najbardziej pustą paczkę
+            # zapamiętujemy najbardziej pustą paczkę
             if puste_kg_paczki > najleżejsza_paczka:
                 najleżejsza_paczka = puste_kg_paczki
                 numer_paczki = liczba_paczek_wyslanych
@@ -31,6 +32,14 @@ while ilosc_sztuk > 0:
             liczba_paczek_wyslanych += 1
             waga_paczki = 0 + waga_elementu
         ilosc_sztuk -= 1
+# sumujemy puste kilogramy z powietrzem ostatniej!! paczki
+# zapamiętujemy najbardziej pustą paczkę
+puste_kg_paczki = 20 - waga_paczki
+suma_pustych_kg += puste_kg_paczki
+if puste_kg_paczki > najleżejsza_paczka:
+    najleżejsza_paczka = puste_kg_paczki
+    numer_paczki = liczba_paczek_wyslanych
+
 print("liczba paczek wysłałnych ", format(liczba_paczek_wyslanych))
 print("liczba kilogramow wyslanych ", format(suma_kilogrmow_wyslanych))
 print("suma pustych kilogramow ", format(suma_pustych_kg))
